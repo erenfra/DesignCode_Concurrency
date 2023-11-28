8bb81bcbc3e51a657a120c1f74db8fd4885dda86
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-11-27 18:03:54 -0600

    Initial Commit

---
+++ DesignCode_Concurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignCode_Concurrency
+//
+//  Created by Renato Oliveira Fraga on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignCode_Concurrency/DesignCode_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode_ConcurrencyApp.swift
+//  DesignCode_Concurrency
+//
+//  Created by Renato Oliveira Fraga on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
